e60ebca5fca2bab89d1e743f2de73f3dc3beae53
petewhartnett <petewhartnett@outlook.com>
2019-11-06 12:15:56 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var kids = prompt('how many kids do you have');
+
+console.log('number of kids:', kids);
+
+var guitars = prompt('how many guitars do you have?');
+console.log('num guitars:', guitars)
+
+if (kids === '3' || kids === '4'){
+console.log('great');
+
+}
+
+if (guitars === '3' && kids ==='3'){
+
+    console.log('cool');
+}
