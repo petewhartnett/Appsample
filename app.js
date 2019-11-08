655d77de78acac940ac51854b69ab54bc88c87f0
Romell Pineda <romell.pineda@gmail.com>
2019-11-07 16:02:17 -0800

    wrapped in functions

--- app.js
+++ app.js
@@ -1,163 +1,179 @@
 'use strict';
 var counter = 0;
+function question1() {
 
-// initial prompt to welcome the user and get their name
-var username = prompt('Welcome to the guessing game quiz!!! What is your name?');
-// a message to the user welcoming them by name and displaying game rules
+    // initial prompt to welcome the user and get their name
+    var username = prompt('Welcome to the guessing game quiz!!! What is your name?');
+    // a message to the user welcoming them by name and displaying game rules
 
 
-//if user doesn't provide name the alert will just not input the name instead of saying NULL
-if (username) {
-    alert(' Welcome ' + username + ' Please answer each question with either yes or no, or y / n');
-}
-else {
-    alert('Please answer each question with either yes or no, or y / n');
-}
-// kids will hold the user promt while uKids will hold the user promt in uppercase
-var kids = prompt('Does Peter have 1 child?');
-var uKids = kids.toUpperCase();
-console.log('Number of children question:', uKids);
+    //if user doesn't provide name the alert will just not input the name instead of saying NULL
+    if (username) {
+        alert(' Welcome ' + username + ' Please answer each question with either yes or no, or y / n');
+    }
+    else {
+        alert('Please answer each question with either yes or no, or y / n');
+    }
+    // kids will hold the user promt while uKids will hold the user promt in uppercase
+    var kids = prompt('Does Peter have 1 child?');
+    var uKids = kids.toUpperCase();
+    console.log('Number of children question:', uKids);
 
-//if else statement that gives the user feed back if they are correct or wrong
-if (uKids === 'YES' || uKids === 'Y') {
-    alert('Correct!');
+    //if else statement that gives the user feed back if they are correct or wrong
+    if (uKids === 'YES' || uKids === 'Y') {
+        alert('Correct!');
+    }
+    else {
+        alert('Wrong! Peter does have 1 child')
+        counter++;
+    }
 }
-else {
-    alert('Wrong! Peter does have 1 child')
-    counter++;
-}
+
 // start of user prompt for question 2
-var dogs = prompt('Does Peter have 2 dogs?');
-var uDogs = dogs.toUpperCase();
-console.log('Number of dogs question:', uDogs);
+function question2() {
+    var dogs = prompt('Does Peter have 2 dogs?');
+    var uDogs = dogs.toUpperCase();
+    console.log('Number of dogs question:', uDogs);
 
-if (uDogs === 'NO' || uDogs === 'N') {
-    alert('Correct! He has 3 dogs');
+    if (uDogs === 'NO' || uDogs === 'N') {
+        alert('Correct! He has 3 dogs');
+    }
+    else {
+        alert('Wrong! He has 3 dogs');
+        counter++;
+    }
 }
-else {
-    alert('Wrong! He has 3 dogs');
-    counter++;
-}
+
 //start of user prompt for question 3
-var outdoors = prompt('Does Peter enjoy the outdoors?');
-var uOutdoors = outdoors.toUpperCase();
-console.log('Enjoy Outdoors Question:', uOutdoors);
+function question3() {
+    var outdoors = prompt('Does Peter enjoy the outdoors?');
+    var uOutdoors = outdoors.toUpperCase();
+    console.log('Enjoy Outdoors Question:', uOutdoors);
 
-if (uOutdoors === 'YES' || uOutdoors === 'Y') {
-    alert('Correct!');
+    if (uOutdoors === 'YES' || uOutdoors === 'Y') {
+        alert('Correct!');
+    }
+    else {
+        alert('Wrong! Peter does enjoy the outdoors');
+        counter++;
+    }
 }
-else {
-    alert('Wrong! Peter does enjoy the outdoors');
-    counter++;
-}
+
 //start of user prompt for question 4
-var family = prompt('Does Peter enjoy spending time with family?');
-var uFamily = family.toUpperCase();
-console.log('Family Time Question:', uFamily);
+function question4() {
+    var family = prompt('Does Peter enjoy spending time with family?');
+    var uFamily = family.toUpperCase();
+    console.log('Family Time Question:', uFamily);
 
-if (uFamily === 'YES' || uFamily === 'Y') {
-    alert('Correct!');
+    if (uFamily === 'YES' || uFamily === 'Y') {
+        alert('Correct!');
+    }
+    else {
+        alert('Wrong! He does enjoy spending time with his family');
+        counter++;
+    }
 }
-else {
-    alert('Wrong! He does enjoy spending time with his family');
-    counter++;
-}
+
 //start of user prompt for question 5
-var commute = prompt('Does Peter have a short commute in the mornings?');
-var uCommute = commute.toUpperCase();
-console.log('Commute Question', uCommute);
+function question5() {
+    var commute = prompt('Does Peter have a short commute in the mornings?');
+    var uCommute = commute.toUpperCase();
+    console.log('Commute Question', uCommute);
 
-if (uCommute === 'NO' || uCommute === 'N') {
-    alert('Correct, he does not have a short commute');
+    if (uCommute === 'NO' || uCommute === 'N') {
+        alert('Correct, he does not have a short commute');
+    }
+    else {
+        alert('Wrong! He has a long commute');
+        counter++;
+    }
 }
-else {
-    alert('Wrong! He has a long commute');
-    counter++;
-}
-
 
 //start of question 6
-var number;
-//counter will count misses specifically for the while loop
-var counter2 = 1;
-while (number !== 5) {
+function question6() {
+    var number;
+    //counter will count misses specifically for the while loop
+    var counter2 = 1;
+    while (number !== 5) {
 
-    //parse int turns word form of a number to a number
-    number = parseInt(prompt('what is peters favorite number?'));
-    console.log('Number Guess: ' + number);
-    // this alerts the user if they did not enter a number
-    if (isNaN(number)) {
-        alert('not a number please enter a number');
-    }
-    // if the number is low user message
-    if (number < 5) {
-        alert('you guessed too low');
-        counter2++;
-    }
-    //if the number is high user message
-    else if (number > 5) {
-        alert('you guessed too high');
-        counter2++;
-    }
-    //if user guesses wrong 4 times break the while loop
-    if (counter2 === 4) {
-        break;
-    }
-
-}
-// will tell user if they failed or passed question based on 4 guess limit
-if (counter2 === 4) {
-    alert('you failed the number game, the answer ws 5!');
-    counter++;
-}
-else {
-    alert('You guessed correctly the answer is 5!');
-}
-
-
-//start of 7th question
-// array holding favorite sports
-var arr = ['soccer', 'football', 'cycling', 'weightlifting', 'hiking'];
-var test = false;
-var counter3 = 0;
-//counter3 will count attempts 6 misses possible 
-while (counter3 != 6) {
-
-    var sports = prompt('what is one of peters favorite sports? You only get 6 Guesses').toLocaleLowerCase();
-    console.log('sports Question: ' + sports);
-    //forloop that will test users input to the sports in the array
-    for (var i = 0; i < arr.length; i++) {
-
-        if (sports === arr[i]) {
-            alert('Correct!!! ' + sports + ' is a favorite sport of his.');
-            //test is a boolean value used as a break case when a correct answer is said it will change to true
-            test = true;
+        //parse int turns word form of a number to a number
+        number = parseInt(prompt('what is peters favorite number?'));
+        console.log('Number Guess: ' + number);
+        // this alerts the user if they did not enter a number
+        if (isNaN(number)) {
+            alert('not a number please enter a number');
+        }
+        // if the number is low user message
+        if (number < 5) {
+            alert('you guessed too low');
+            counter2++;
+        }
+        //if the number is high user message
+        else if (number > 5) {
+            alert('you guessed too high');
+            counter2++;
+        }
+        //if user guesses wrong 4 times break the while loop
+        if (counter2 === 4) {
             break;
         }
 
     }
-    //if win requirements made then break if not count and tell them how many misses
-    if (test === true) {
-        break;
+    // will tell user if they failed or passed question based on 4 guess limit
+    if (counter2 === 4) {
+        alert('you failed the number game, the answer ws 5!');
+        counter++;
     }
     else {
-        counter3++;
-        alert('Wrong! ' + counter3 + ' Guesses out of 6!');
+        alert('You guessed correctly the answer is 5!');
     }
 }
-// if user misses 6 they will be booted from while loop and this will alert result and count overall misses
-if (counter3 === 6) {
-    alert('You failed this question.');
-    counter++;
-}
+
+//start of 7th question
+// array holding favorite sports
+function question7() {
+    var arr = ['soccer', 'football', 'cycling', 'weightlifting', 'hiking'];
+    var test = false;
+    var counter3 = 0;
+    //counter3 will count attempts 6 misses possible 
+    while (counter3 != 6) {
+
+        var sports = prompt('what is one of peters favorite sports? You only get 6 Guesses').toLocaleLowerCase();
+        console.log('sports Question: ' + sports);
+        //forloop that will test users input to the sports in the array
+        for (var i = 0; i < arr.length; i++) {
+
+            if (sports === arr[i]) {
+                alert('Correct!!! ' + sports + ' is a favorite sport of his.');
+                //test is a boolean value used as a break case when a correct answer is said it will change to true
+                test = true;
+                break;
+            }
+
+        }
+        //if win requirements made then break if not count and tell them how many misses
+        if (test === true) {
+            break;
+        }
+        else {
+            counter3++;
+            alert('Wrong! ' + counter3 + ' Guesses out of 6!');
+        }
+    }
+    // if user misses 6 they will be booted from while loop and this will alert result and count overall misses
+    if (counter3 === 6) {
+        alert('You failed this question.');
+        counter++;
+    }
 
 
-//final message thanking person by name for playing and give total missed based on counter
+    //final message thanking person by name for playing and give total missed based on counter
 
-if (username) {
-    alert(' Thanks for playing ' + username + '! Out of 7 question you missed ' + counter + '! ' + (7 - counter) + ' correct answers, not bad!');
-}
-else {
-    alert(' Thanks for playing! Out of 7 question you missed ' + counter + '! ' + (7 - counter) + ' correct answers, not bad!');
+    if (username) {
+        alert(' Thanks for playing ' + username + '! Out of 7 question you missed ' + counter + '! ' + (7 - counter) + ' correct answers, not bad!');
+    }
+    else {
+        alert(' Thanks for playing! Out of 7 question you missed ' + counter + '! ' + (7 - counter) + ' correct answers, not bad!');
 
+    }
 }
